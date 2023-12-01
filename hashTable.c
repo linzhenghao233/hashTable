7d043f8090f25646d3b54027d69a53da56edbb05
linzhenghao233 <1392872316@qq.com>
2023-12-01 21:04:29 +0800

    1201

--- hashTable.c
+++ hashTable.c
@@ -26,3 +26,54 @@ int hashFunction(int key) {
 }
 
 //插入键值对到哈希表
+void insert(int key, int value) {
+	int index = hashFunction(key);
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	if (newNode == NULL) {
+		printf("Memory allocation failed. Exiting...\n");
+		return; // 或者采取其他错误处理方式
+	}
+	newNode->key = key;
+	newNode->value = value;
+	newNode->next = NULL;
+
+	if (hashTable[index] == NULL) {
+		hashTable[index] = newNode;
+	}
+	else {
+		struct Node* temp = hashTable[index];
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+}
+
+//查找键对应的值
+int search(int key) {
+	int index = hashFunction(key);
+
+	struct Node* temp = hashTable[index];
+	while (temp != NULL) {
+		if (temp->key == key) {
+			return temp->value;
+		}
+		temp = temp->next;
+	}
+	return -1;//没有找到
+}
+
+int main(void) {
+	initHashTable();
+
+	insert(5, 10);
+	insert(2, 20);
+	insert(15, 30);
+
+	printf("Value for key 5: %d\n", search(5));	//应该输出10
+	printf("Value for key 15: %d\n", search(15));	//应该输出30
+	printf("Value for key 8: %d\n", search(8));	//应该输出-1（未找到）
+
+	return 0;
+}
